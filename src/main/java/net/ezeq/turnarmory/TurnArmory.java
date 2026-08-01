f411d6f6667d4dd5f9568cd6213ab1b6d08dc3da
Ezequiestral45 <ezequiel.thon@gmail.com>
2026-08-01 07:46:11 -0400

    Bones of the Armory

---
+++ src/main/java/net/ezeq/turnarmory/TurnArmory.java
@@ -0,0 +1,23 @@
+package net.ezeq.turnarmory;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.util.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TurnArmory implements ModInitializer {
+	public static final String MOD_ID = "turn-armory";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Forging an Armory!");
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.of(MOD_ID, path);
+	}
+}
---
+++ src/main/java/net/ezeq/turnarmory/TurnArmoryClient.java
@@ -0,0 +1,10 @@
+package net.ezeq.turnarmory;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TurnArmoryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
